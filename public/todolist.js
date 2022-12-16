1c3ac06129813054ef7613832d2ef873aba56104
hellogyeol <hellogyeol@gmail.com>
2022-12-16 23:56:03 +0900

    test: todo 생성 테스트

--- public/todolist.js
+++ public/todolist.js
@@ -1,1 +1,11 @@
-console.log('test');
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const ul = document.querySelector('.ul');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const li = document.createElement('li');
+  li.innerText = input.value;
+  ul.append(li);
+  input.value = '';
+});
