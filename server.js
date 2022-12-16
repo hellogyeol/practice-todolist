6d756ce8259d1bc43dfd46a238171d63aa2ba6c2
hellogyeol <hellogyeol@gmail.com>
2022-12-16 23:24:03 +0900

    server: index.html에 정적 파일 연결

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/todolist.html`);
 });
 
 app.listen(port, () => {
