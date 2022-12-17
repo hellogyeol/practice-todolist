129a2a388cb23a8d15896d704c25361bb86d0346
hellogyeol <hellogyeol@gmail.com>
2022-12-17 15:44:22 +0900

    feat: todo 로컬 스토리지 저장

--- public/todolist.js
+++ public/todolist.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
 const ul = document.querySelector('.ul');
+let todolist = [];
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const li = document.createElement('li');
-  li.innerText = input.value;
-  ul.append(li);
+  todolist.push(input.value);
+  console.log(todolist);
   input.value = '';
+  localStorage.setItem('todolist', JSON.stringify(todolist));
 });
