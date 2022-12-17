24e23292b563c92aee53cbbfd9b214c7d644cddb
hellogyeol <hellogyeol@gmail.com>
2022-12-17 19:57:16 +0900

    feat: 새로고침 후에도 todolist 유지

--- public/todolist.js
+++ public/todolist.js
@@ -1,7 +1,12 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.input');
 const ul = document.querySelector('.ul');
+const savedList = localStorage.getItem('todolist');
 let todolist = [];
+
+if (savedList !== null) {
+  todolist = JSON.parse(savedList);
+}
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
