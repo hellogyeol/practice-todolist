8def65cd8bf107c23db178c319f722fa4f6597cd
hellogyeol <hellogyeol@gmail.com>
2022-12-18 23:39:08 +0900

    feat: Todo 저장, 렌더링, 새로고침 시에도 유지

--- public/todolist.js
+++ public/todolist.js
@@ -6,20 +6,37 @@ let todolist = [];
 
 if (savedList !== null) {
   todolist = JSON.parse(savedList);
-  saveList();
+  renderList();
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const todo = {
     id: String(Date.now()),
     content: input.value
   };
   input.value = '';
-  todolist.push(todo)
+  todolist.push(todo);
+  
   saveList();
+  pageRefresh();
+  renderList();
 });
 
 function saveList() {
   localStorage.setItem('todolist', JSON.stringify(todolist));
+}
+
+function pageRefresh() {
+  location.reload();
+}
+
+function renderList() {
+  todolist.map(todo => {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.innerText = todo.content;
+    li.append(span);
+    ul.append(li);
+  });
 }
